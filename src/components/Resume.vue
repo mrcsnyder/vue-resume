bbe57eadb2cd0947bac21db6b411b267357c406f
chrissnyder <snyder.chris.m@gmail.com>
2020-03-23 19:15:40 -0700

    Added ResumeSkills and GET capabilities using fetch to the endpoint

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -6,324 +6,16 @@
             <div class="row">
 
                 <div class="col-lg-8 col-12 mt-2">
-
                    <resume-work :work="work"></resume-work>
                    <resume-education :education="education"/>
                    <resume-awards :awards="awards"/>
-
-
                 </div>
 
                 <div class="col-lg-4 col-12">
-                    <h2 class="trebuchet lg-headers text-center">Skills <i class="fas fa-code"></i></h2>
-
-                    <h5 class="trebuchet text-muted">Coding</h5>
-                    <table class="table calibri">
-
-                        <tbody>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Aptitude to Learn Unlisted Skill</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:98%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">ASP.NET MVC</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:75%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Bootstrap 3/4</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">C#</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:83%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">HTML5/CSS3</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Java</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:70%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Javascript</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:92%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">jQuery</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Laravel</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">PHP</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">Responsive Design</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:90%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Ruby</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">Ruby on Rails</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">SQL</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Symfony</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">Visual Basic</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:75%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Vue.js</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:75%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Wordpress</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        </tbody>
-                    </table>
-
-                    <h5 class="trebuchet text-muted">Methodologies/DevOps</h5>
-                    <table class="table calibri">
-
-                        <tbody>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Agile Development</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Docker</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">LEMP</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">VPS</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-
-
-                        </tbody>
-                    </table>
-
-
-                    <h5 class="trebuchet text-muted">Software</h5>
-                    <table class="table calibri">
-
-                        <tbody>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Adobe Flash</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:70%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Adobe Photoshop</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:75%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Git/Github</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:90%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">JetBrains IDE's</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Legacy Systems</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:85%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Microsoft Office</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:90%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Microsoft Project</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:70%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Microsoft Visio</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:70%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">SQL Server</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:75%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Tableau</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Tower</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Visual Studio</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">VS Code</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-
-                        </tbody>
-                    </table>
-
-                    <h5 class="trebuchet text-muted">Operating Systems</h5>
-                    <table class="table calibri">
-
-                        <tbody>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Android</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:90%;"></div></div></td>
-                        </tr>
-                        <tr class="d-flex">
-                            <td class="col-6">iOS</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Mac</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Windows</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-
-                        </tbody>
-
-                    </table>
-
-                    <h5 class="trebuchet text-muted">Business</h5>
-                    <table class="table calibri">
-
-                        <tbody>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Ability to Meet Deadlines</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Attention to Detail</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Collaboration</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Customer Service</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Learning New Skills</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Management Experience</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:80%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Organization</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:98%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Phone Etiquette</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:95%;"></div></div></td>
-                        </tr>
-
-
-                        <tr class="d-flex">
-                            <td class="col-6">Responsible</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Team work</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:100%;"></div></div></td>
-                        </tr>
-
-                        <tr class="d-flex">
-                            <td class="col-6">Typing <br/>(90+ WPM)</td>
-                            <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" style="width:99%;"></div></div></td>
-                        </tr>
-
-
-                        </tbody>
-                    </table>
-
+                    <resume-skills :skills="skills"/>
                 </div>
 
             </div>
-
 
     </div>
 </template>
@@ -333,6 +25,8 @@
     import ResumeWork from "./ResumeWork";
     import ResumeEducation from "./ResumeEducation";
     import ResumeAwards from "./ResumeAwards";
+    import ResumeSkills from "./ResumeSkills";
+
 
 
     export default {
@@ -343,6 +37,7 @@
                 work: [],
                 education: [],
                 awards: {scholarships: [], honors: []},
+                skills: {coding: [], methods_devops: [], software: [], operating_systems: [], business: [] },
                 urlPre: 'http://resume-api.thisdudecodes.com/api/'
 
             }
@@ -352,6 +47,7 @@
             ResumeEducation,
             ResumeAwards,
             ResumeWork,
+            ResumeSkills,
 
             },
 
@@ -381,6 +77,39 @@
                         this.work = data;
 
                         console.log('test'+ this.work);
+                    });
+
+
+            },
+
+            //get skills from endpoint
+            getSkills() {
+
+                const url = this.urlPre + 'skills';
+
+                fetch(url, {
+                    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                    // mode: 'no-cors', // no-cors, *cors, same-origin
+                    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    // credentials: 'same-origin', // include, *same-origin, omit
+                    headers: {
+                        'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    }
+                })
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+
+                        this.skills = data;
+                        this.skills.coding = data.coding;
+                        this.skills.methods_devops = data.methods_devops;
+                        this.skills.software = data.software;
+                        this.skills.operating_systems = data.operating_systems;
+                        this.skills.business = data.business;
+
+                        console.log(this.skills);
                     });
 
 
@@ -447,6 +176,7 @@
         mounted() {
 
            this.getWork();
+           this.getSkills();
            this.getEducation();
            this.getAwards();
 
---
+++ src/components/ResumeSkills.vue
@@ -0,0 +1,113 @@
+<template>
+
+    <div>
+
+        <h2 class="trebuchet lg-headers text-center">Skills <i class="fas fa-code"></i></h2>
+
+        <div v-if="skills.coding.length > 0">
+            <h5 class="trebuchet text-muted">Coding</h5>
+            <table class="table calibri">
+
+                <tbody>
+                <tr v-for="code in skills.coding" :key="code.id" class="d-flex">
+                    <td class="col-6">{{code.skill}}</td>
+                    <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" :style="'width:'+code.rating+'%;'"></div></div></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div v-if="skills.methods_devops.length > 0">
+            <h5 class="trebuchet text-muted">Methodologies/DevOps</h5>
+            <table class="table calibri">
+
+                <tbody>
+                <tr v-for="method in skills.methods_devops" :key="method.id" class="d-flex">
+                    <td class="col-6">{{method.skill}}</td>
+                    <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" :style="'width:'+method.rating+'%;'"></div></div></td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+        <div v-if="skills.software.length > 0">
+            <h5 class="trebuchet text-muted">Software</h5>
+            <table class="table calibri">
+
+                <tbody>
+                <tr v-for="software in skills.software" :key="software.id" class="d-flex">
+                    <td class="col-6">{{software.skill}}</td>
+                    <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" :style="'width:'+software.rating+'%;'"></div></div></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+        <div v-if="skills.operating_systems.length > 0">
+            <h5 class="trebuchet text-muted">Operating Systems</h5>
+            <table class="table calibri">
+
+                <tbody>
+                <tr v-for="operating_system in skills.operating_systems" :key="operating_system.id" class="d-flex">
+                    <td class="col-6">{{operating_system.skill}}</td>
+                    <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" :style="'width:'+operating_system.rating+'%;'"></div></div></td>
+                </tr>
+                </tbody>
+
+            </table>
+
+        </div>
+
+        <div v-if="skills.business.length > 0">
+            <h5 class="trebuchet text-muted">Business</h5>
+            <table class="table calibri">
+
+                <tbody>
+                <tr v-for="biz in skills.business" :key="biz.id" class="d-flex">
+                    <td class="col-6">{{biz.skill}}</td>
+                    <td class="col-6"><div class="rating"><div class="stars"></div><div class="back" :style="'width:'+biz.rating+'%;'"></div></div></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+
+    export default {
+        name: 'ResumeSkills',
+
+        components: {
+
+
+        },
+
+        data(){
+            return{
+
+            }
+
+        },
+
+        props:{
+
+            skills: Object,
+
+        },
+
+        methods:{
+            
+        },
+
+        mounted() {
+
+        }
+
+    }
+</script>
