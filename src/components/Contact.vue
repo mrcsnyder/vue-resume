6af4905bf57ef6240b6aa6c415bade3148273973
chrissnyder <snyder.chris.m@gmail.com>
2020-04-02 23:10:26 -0700

    improved functionality of validation for contact page/form & further improved UX

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -23,7 +23,7 @@
                     <label class="resume-label" for="name">Name</label>
                    <span>
                        <br/>
-                       <label v-if="errors.name" class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.name[0] }}</label>
+                       <label v-if="this.errors.name && this.name === '' " class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.name[0] }}</label>
                    </span>
                     <div data-role="fieldcontainer">
                         <input class="form-control" type="text" id="name" name="name" v-model="name" placeholder="Please Enter Your Name"/>
@@ -35,11 +35,13 @@
                     <label class="resume-label" for="email">E-mail</label>
                     <span>
                        <br/>
-                       <label v-if="errors.email" class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.email[0] }}</label>
-                   </span>
+                       <label v-if="this.errors.email && this.email === '' " class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.email[0] }}</label>
+                       <label v-else-if="this.errors.email && !validEmail() " class="error"><b-icon icon="alert-circle-fill" variant="dark"/> Please enter in your real email address!</label>
+
+                    </span>
 
                     <div data-role="fieldcontainer">
-                        <input class="form-control" type="email" id="email" name="email" v-model="email" placeholder="Please Enter Your Email"/>
+                        <input class="form-control" type="email" novalidate="true" id="email" name="email" v-model="email" placeholder="Please Enter Your Email"/>
                     </div>
 
                 </div>
@@ -48,7 +50,7 @@
                     <label class="resume-label" for="message">Message</label>
                     <span>
                        <br/>
-                       <label v-if="errors.message" class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.message[0] }}</label>
+                       <label v-if="this.errors.message && this.message === '' " class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.message[0] }}</label>
                    </span>
                     <div data-role="fieldcontainer">
                         <textarea class="form-control" id="message" name="message" v-model="message"
@@ -58,10 +60,10 @@
             </div>
         <span>
             <br/>
-            <label v-if="errors.captyResponse" class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.captyResponse[0] }}</label>
+            <label v-if="this.errors.captyResponse && this.captchaResponse === '' " class="error"><b-icon icon="alert-circle-fill" variant="dark"/> {{ errors.captyResponse[0] }}</label>
+
         </span>
             <div id="captyCapn" class="g-recaptcha" data-sitekey="6Lc6uOUUAAAAAD06XtOzaXtr-2DoC15zQ4fh0mj4"></div>
-
             <br/>
             <input class="btn btn-lg btn-dark" type="submit" @click="sendContact()" value="Send Message" id="submit"/>
             <br/>
@@ -90,10 +92,11 @@
             return{
 
                 message: '',
+                captchaResponse: '',
                 email: '',
+                emailFormat: false,
                 name: '',
                 errors: [],
-
 
             }
 
@@ -105,10 +108,10 @@
 
                 let captchaDiv = document.getElementById('captyCapn');
 
-                let captchaResponse = captchaDiv.querySelector('.g-recaptcha-response').value;
+                this.captchaResponse = captchaDiv.querySelector('.g-recaptcha-response').value;
 
 
-                const data = { name: this.name, email: this.email, message: this.message, captyResponse: captchaResponse };
+                const data = { name: this.name, email: this.email, message: this.message, captyResponse: this.captchaResponse };
 
                 fetch('http://resume-api.thisdudecodes.com/resume/send-contact', {
                     method: 'POST',
@@ -144,13 +147,19 @@
                 script.setAttribute( 'src','https://www.google.com/recaptcha/api.js?render=explicit"')
                 document.getElementsByTagName('head')[0].appendChild(script)
 
+            },
+
+            validEmail() {
+
+                let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                return (this.email === '')? '' : (re.test(this.email)) ? this.emailFormat = true : this.emailFormat = false;
             }
         },
 
         mounted() {
             // create reCaptcha async defer with createRecaptcha function
             this.createRecaptcha()
-
         },
 
 
